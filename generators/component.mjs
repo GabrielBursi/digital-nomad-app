69b897735b9b362a6e00c2fb2709d4125f0c34ee
Gabriel Bursi <gabriellbursii@gmail.com>
2025-05-28 23:27:24 -0300

    chore: eslint, prettier, husky, typescript, generators

---
+++ generators/component.mjs
@@ -0,0 +1,34 @@
+export default function (plop) {
+	plop.setGenerator('component', {
+		description: 'Create a component',
+		prompts: [
+			{
+				type: 'input',
+				name: 'name',
+				message: 'Nome do componente:',
+			},
+			{
+				type: 'confirm',
+				name: 'useProps',
+				message: 'Vai usar type para as propriedades do componente?',
+			},
+		],
+		actions: [
+			{
+				type: 'add',
+				path: '../components/{{pascalCase name}}/{{pascalCase name}}.tsx',
+				templateFile: 'templates/components/index.tsx.hbs',
+			},
+			{
+				type: 'add',
+				path: '../components/{{pascalCase name}}/{{pascalCase name}}.spec.tsx',
+				templateFile: 'templates/components/test.spec.tsx.hbs',
+			},
+			{
+				type: 'add',
+				path: '../components/{{pascalCase name}}/{{pascalCase name}}.types.ts',
+				templateFile: 'templates/components/types.ts.hbs',
+			},
+		],
+	})
+}
---
+++ generators/screen.mjs
@@ -0,0 +1,34 @@
+export default function (plop) {
+	plop.setGenerator('screen', {
+		description: 'Create a screen',
+		prompts: [
+			{
+				type: 'input',
+				name: 'name',
+				message: 'Nome da página:',
+			},
+			{
+				type: 'confirm',
+				name: 'useProps',
+				message: 'Vai usar type para as propriedades da página?',
+			},
+		],
+		actions: [
+			{
+				type: 'add',
+				path: '../screens/{{pascalCase name}}//{{pascalCase name}}.tsx',
+				templateFile: 'templates/screen/index.tsx.hbs',
+			},
+			{
+				type: 'add',
+				path: '../screens/{{pascalCase name}}/{{pascalCase name}}.spec.tsx',
+				templateFile: 'templates/screen/test.spec.tsx.hbs',
+			},
+			{
+				type: 'add',
+				path: '../screens/{{pascalCase name}}//{{pascalCase name}}.types.ts',
+				templateFile: 'templates/screen/types.ts.hbs',
+			},
+		],
+	})
+}
