736882a9cc7b82103f6cfd05a8a3b0157fe8ccfb
Gabriel Bursi <gabriellbursii@gmail.com>
2025-06-07 14:16:38 -0300

    feat(city): city card component

---
+++ src/components/app/CityCard/CityCard.tsx
@@ -0,0 +1,45 @@
+import React, { memo } from 'react'
+import { ImageBackground, StyleSheet } from 'react-native'
+
+import { Box, Icon, Text } from '@/components'
+import { useAppTheme } from '@/hooks'
+
+import { CityCardProps } from './CityCard.types'
+
+const CityCardMemoized = ({ cityPreview }: Readonly<CityCardProps>) => {
+	const { borderRadii } = useAppTheme()
+	return (
+		<ImageBackground
+			source={cityPreview.coverImage}
+			style={styles.cityImage}
+			imageStyle={{ borderRadius: borderRadii.default }}
+		>
+			<Box
+				position="absolute"
+				width="100%"
+				height="100%"
+				backgroundColor="midnightBlack"
+				opacity={0.25}
+			/>
+			<Box flex={1} padding="s24" justifyContent="space-between">
+				<Box alignSelf="flex-end">
+					<Icon name="Favorite-outline" color="text" />
+				</Box>
+
+				<Box>
+					<Text variant="title22">{cityPreview.name}</Text>
+					<Text variant="text16">{cityPreview.country}</Text>
+				</Box>
+			</Box>
+		</ImageBackground>
+	)
+}
+
+const styles = StyleSheet.create({
+	cityImage: {
+		height: 280,
+		width: '100%',
+	},
+})
+
+export const CityCard = memo(CityCardMemoized)
---
+++ src/components/app/CityCard/CityCard.types.ts
@@ -0,0 +1,3 @@
+import type { CityPreview } from '@/types/city'
+
+export type CityCardProps = { cityPreview: CityPreview }
