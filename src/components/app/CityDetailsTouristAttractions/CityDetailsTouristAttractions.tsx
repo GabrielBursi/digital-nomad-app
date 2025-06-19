48d3f9f77698c0bc773f47f7ff09d0028f6b18de
Gabriel Bursi <gabriellbursii@gmail.com>
2025-06-19 17:03:38 -0300

    feat(city): city details tourist attractions component

---
+++ src/components/app/CityDetailsTouristAttractions/CityDetailsTouristAttractions.tsx
@@ -0,0 +1,30 @@
+import React, { memo } from 'react'
+
+import { Accordion, Box, Text } from '@/components'
+
+import type { CityDetailsTouristAttractionsProps } from './CityDetailsTouristAttractions.types'
+
+const CityDetailsTouristAttractionsMemoized = ({
+	touristAttractions,
+}: Readonly<CityDetailsTouristAttractionsProps>) => {
+	return (
+		<Box padding="padding">
+			<Text variant="title22" mb="s8">
+				Pontos turísticos
+			</Text>
+			<Box gap="s8">
+				{touristAttractions.map((attraction) => (
+					<Accordion
+						key={attraction.id}
+						title={attraction.name}
+						description={attraction.description}
+					/>
+				))}
+			</Box>
+		</Box>
+	)
+}
+
+export const CityDetailsTouristAttractions = memo(
+	CityDetailsTouristAttractionsMemoized
+)
---
+++ src/components/app/CityDetailsTouristAttractions/CityDetailsTouristAttractions.types.ts
@@ -0,0 +1,6 @@
+import { City } from '@/types/city'
+
+export type CityDetailsTouristAttractionsProps = Pick<
+	City,
+	'touristAttractions'
+>
