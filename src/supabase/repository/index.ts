55e8fc2aeabd079fb9cbcfd977d1599a87722c87
Gabriel Bursi <gabriellbursii@gmail.com>
2025-07-12 14:16:38 -0300

    feat(supabase): supabase repositories

--- src/supabase/repository/index.ts
+++ src/supabase/repository/index.ts
@@ -1,2 +1,12 @@
+import { Repositories } from '@/domain/Repositories'
+
+import { supabaseCategoryRepo } from './category'
+import { supabaseCityRepo } from './city'
+
 export * from './category'
 export * from './city'
+
+export const SupabaseRepositories: Repositories = {
+	city: supabaseCityRepo,
+	category: supabaseCategoryRepo,
+}
