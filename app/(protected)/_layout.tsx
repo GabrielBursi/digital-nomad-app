e195cc86a30042ef607c1dff71a53428b631eef6
Gabriel Bursi <gabriellbursii@gmail.com>
2025-07-19 13:36:25 -0300

    refactor(auth): protect app layout with auth context

--- app/(protected)/_layout.tsx
+++ app/(protected)/_layout.tsx
@@ -1,10 +1,17 @@
 import { Redirect, Stack } from 'expo-router'
 
+import { useAuthContext } from '@/domain/auth/context'
+
 import 'react-native-reanimated'
 
-const isSignedIn = false
 export default function ProtectedLayout() {
-	if (isSignedIn) {
+	const { authUser, isReady } = useAuthContext()
+
+	if (!isReady) {
+		return null
+	}
+
+	if (authUser) {
 		return <Redirect href="../sign-in" />
 	}
 	return (
