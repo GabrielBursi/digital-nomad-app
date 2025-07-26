f5967d5e81865b2dc41a20209c70b494abf04262
Gabriel Bursi <gabriellbursii@gmail.com>
2025-07-26 14:45:52 -0300

    feat(auth): reset password route

---
+++ src/screens/ResetPassword/ResetPassword.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { Screen, Text } from '@/components'
+
+export const ResetPasswordScreen = () => {
+	return (
+		<Screen>
+			<Text>ResetPassword</Text>
+		</Screen>
+	)
+}
