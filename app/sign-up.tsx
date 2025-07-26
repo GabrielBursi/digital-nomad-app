f20d77a4242002a6278566a638627f8390ccaec0
Gabriel Bursi <gabriellbursii@gmail.com>
2025-07-26 14:48:28 -0300

    feat(auth): sign up route

---
+++ app/sign-up.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import { SignUpScreen } from '@/screens'
+
+export default function SignUp() {
+	return <SignUpScreen />
+}
---
+++ src/screens/SignUp/SignUp.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { Screen, Text } from '@/components'
+
+export const SignUpScreen = () => {
+	return (
+		<Screen>
+			<Text>SignUp</Text>
+		</Screen>
+	)
+}
